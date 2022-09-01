86349f8308450e586b2a3b875e22a2a65bcd0688
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-09-01 23:28:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-
-
-//.then and .catch ki help se data ko fetch krna
-
-
-
-
-fetch("https://reqres.in/api/users?page=2")
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (res2) {
-    console.log(res2.data);
-    var content = document.getElementById("content");
-    res2.data.forEach(function (element) {
-        console.log(element.avatar,'aman');
-      content.innerHTML += `
- 
-      <div style = "margin:5%">
-      
-      <img src=${element.avatar} alt="preview" >
-  
-      <div id="fullName">${element.first_name} ${element.last_name}</div>
-      <div id="email">${element.email}</div>
-      </div>
-      `
-    })
-  })
-  .catch(function (err) {
+async function getData() {
+  try {
+    let api_key = "fc7a337a";
+    let url = "http://www.omdbapi.com/?i=tt3896198&apikey=${api_key}";
+    let res = await fetch(url);
+    let data = await res.json;
+    console.log(data);
+  } catch (err) {
     console.log(err);
-  });
+  }
+}
+getData();
